9b9637204b6a380abe21e1119a4c8b9fea80e1ca
Luis Miguel Garcia <Gordisandroid@gmail.com>
2024-12-07 19:54:58 -0400

    types and servies

--- backend/service/user/routes.go
+++ backend/service/user/routes.go
@@ -1,6 +1,8 @@
 package user
 
 import (
+	"github.com/Gordisluis/LIGA-DRAFT/types"
+	"api/utils"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -15,7 +17,7 @@ func NewHandler() *Handler {
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/login", h.handleLogin).Methods("POST")
-	router.HandleFunc("/register", h.handleLogin).Methods("POST")
+	router.HandleFunc("/register", h.handleRegister).Methods("POST")
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
@@ -23,5 +25,12 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// obtener JSON carga
+	var payload types.RegisterUserPayLoad
+	if err := utils.ParseJSON(r, payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+	// revisar si el user existe
 
+	// si no crearlos
 }
