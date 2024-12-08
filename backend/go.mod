5ad74771261543743a109bafa989c46b315a6f37
Luis Miguel Garcia <Gordisandroid@gmail.com>
2024-12-08 12:19:29 -0400

    servicios

--- backend/go.mod
+++ backend/go.mod
@@ -2,11 +2,13 @@ module api
 
 go 1.22.4
 
-require github.com/go-sql-driver/mysql v1.8.1
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
+)
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.30.0 // indirect
 )
--- backend/service/user/routes.go
+++ backend/service/user/routes.go
@@ -1,17 +1,21 @@
 package user
 
 import (
-	"api/utils"
-	"net/http"
+	"api/service/auth"
 	"api/types"
+	"api/utils"
+	"fmt"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	Store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(Store types.UserStore) *Handler {
+	return &Handler{Store: Store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -24,10 +28,30 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
-	var user types.RegisterUserPayLoad
-	if err := utils.ParseJSON(r, &user); err != nil {
-		utils.WriteError(w, http.StatusBadRequest, err)
+	_, err = h.Store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("usuario con el email %s ya existe", payload.Email))
+		return 
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
 		return
 	}
+	
+	err = h.Store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatisInternalServerError, err)
+		return
+	}
+	
+	
+	utils.WriteJSON(w, http.StatusCreated, nil)
 
 }
