95071d81694c79baaf61c6a32652360eaa2ac54a
Luis Miguel Garcia <Gordisandroid@gmail.com>
2024-12-08 13:21:50 -0400

    api/users id

--- backend/main.go
+++ backend/main.go
@@ -7,13 +7,37 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type User struct {
+	ID int `json:"id"`
+	Name string `json:"name"`
+	Phone int `json:"phone"`
+}
+
 func main() {
 	fmt.Println("Empezamos a desarrollar")
-
 	app := fiber.New()
+
+	userRegister := []User{}
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
+	})
+
+	app.Post("/api/users", func(c *fiber.Ctx) error {
+		user := &User{}
+
+		if err := c.BodyParser(user); err != nil {
+			return err
+		}
+
+		if user.ID == 0 {
+			return c.Status(400).JSON(fiber.Map{"error":"ID de usuario es requerido"})
+		}
+
+		user.ID = len(userRegister) + 1 
+		userRegister = append(userRegister, *user)
+
+		return c.Status(201).JSON(user)
 	})
 
 	log.Fatal(app.Listen(":4000"))
